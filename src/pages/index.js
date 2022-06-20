d02de4b27f44eab3273b0687f997f29c3e61d0bf
spacecodes1 <106747552+spacecodes1@users.noreply.github.com>
2022-06-20 14:20:54 +0200

    retest

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import LeafletMap from "../components/leafletmap"
 
-const task = await env.TodoList.get("spacecodes:GPS");
+//const task = await env.TodoList.get("spacecodes:GPS");
 
 const IndexPage = () => (
   <Layout>
@@ -12,9 +12,9 @@ const IndexPage = () => (
 
     {typeof window !== 'undefined' &&
         <LeafletMap
-          position={15,-1}
+          position={49.3015556335449,1.0316807031631}
           zoom={15}
-          markerText={task}
+          markerText={"task"}
         />
     }
 
